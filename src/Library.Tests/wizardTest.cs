05ee20db8fd02d9d8253df3d9257246f577c175d
Josue2kMR <josuernesto05@gmail.com>
2024-09-23 11:51:04 -0300

    test

--- src/Library.Tests/wizardTest.cs
+++ src/Library.Tests/wizardTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Items;
 using Library;
 using NUnit.Framework;
 
@@ -8,14 +9,20 @@ namespace Library.Tests;
 [TestOf(typeof(Wizard))]
 public class wizardTest
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
-
     [Test]
-    public void Test1()
+    public void Wizard_Attacks_Dwarf_ReducesHealth()
     {
-        Assert.Pass();
+        // Arrange
+        var axe = new Axe { AttackValue = 30 };  // Asumimos que el hacha tiene un valor de ataque
+        var armor = new Armor { DefenseValue = 20 };  // Asumimos que la armadura tiene un valor de defensa
+        var wizard = new Wizard("Gandalf", 100, axe, armor);
+        var dwarf = new dwarves("Thorin", 100, axe, armor);
+
+        // Act
+        wizard.Attack(dwarf);
+
+        // Assert
+        Assert.True(dwarf.health, 100);
+        Assert.AreEqual(90, dwarf.health);  // Suponiendo que el ataque de Gandalf reduce 10 de salud
     }
 }
