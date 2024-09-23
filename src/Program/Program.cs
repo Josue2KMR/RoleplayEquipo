c1bbea8495f5947ead8a3f6bfcb89322cc8de9d5
mateocimassi <mateo.cimassi@correo.ucu.edu.uy>
2024-09-23 12:05:25 -0300

    update program

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -2,9 +2,19 @@
 using Library;
 using Library.Interfaces;
 
-IItemAttackValue espada = new Sword();
-IItemDefenseValue casco = new Helmet();
-IPersonaje gimli = new dwarves("gimli", 100, espada, casco);
-IPersonaje dobby = new Elfo("dobby", 100, espada, casco);
-gimli.GetDefence();
+namespace RoleplayGame
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IItemAttackValue espada = new Sword();
+            IItemDefenseValue casco = new Helmet();
+            IPersonaje gimli = new dwarves("gimli", 100, espada, casco);
+            IPersonaje franui = new Elfo("franui", 100, espada, casco);
+            gimli.GetDefence();
+        }
+    }
+}
+
 
